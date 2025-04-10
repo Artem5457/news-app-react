9e0a0fad314dbd19838d7a44c3405397074b064a
Artem Bolshak <artembolshak.work@gmail.com>
2025-04-10 21:50:00 +0300

    Added Pagination

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,11 +5,14 @@ import { getNews } from '../../api/apiNews.js';
 import {useLocalStorage} from '../../utils/hooks/useLocalStorage.jsx';
 import { NewsList } from '../../components/NewsList/NewsList.jsx';
 import { SkeletonView } from '../../components/SkeletonView/SkeletonView.jsx';
+import { Pagination } from '@mui/material';
 
 export function Main() {
   const [newsLS, setNewsLS] = useLocalStorage('news', []);
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
 
   useEffect(() => {
     const fetchNews = () => {
@@ -21,17 +24,40 @@ export function Main() {
         console.log(error);
       }
     };
+    // const fetchNews = async () => {
+    //   try {
+    //     setIsLoading(true);
+    //     const response = await getNews(2);
+    //     setNews(response.news);
+    //     setIsLoading(false);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // };
     fetchNews();
   }, [newsLS]);
+
+  const handleChange = (event, value) => {
+    event.preventDefault();
+    setCurrentPage(value);
+  };
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const visibleNews = news.slice(startIndex, endIndex);
 
   return (
     <main className={styles.main}>
       {(news.length > 0 && !isLoading)
-        ? <NewsBanner item={news[0]} />
+        ? <NewsBanner item={visibleNews[0]} />
         : <SkeletonView type="banner"/>
       }
+      <Pagination
+        count={Math.ceil(news.length / itemsPerPage)}
+        page={currentPage}
+        onChange={handleChange}
+      />
       {!isLoading
-        ? <NewsList news={news}/>
+        ? <NewsList news={visibleNews}/>
         : <SkeletonView type="item" count={10}/>
       }
     </main>
